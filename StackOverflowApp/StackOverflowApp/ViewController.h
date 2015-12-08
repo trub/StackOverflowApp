30dab4f5faa18dfa1c6cd84d10d28b11b5010b5a
matthew weintrub <heytrub@gmail.com>
2015-12-07 17:50:00 -0800

    first commit: init repo

---
+++ StackOverflowApp/StackOverflowApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverflowApp
+//
+//  Created by Matthew Weintrub on 12/7/15.
+//  Copyright © 2015 matthew weintrub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackOverflowApp/StackOverflowApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StackOverflowApp
+//
+//  Created by Matthew Weintrub on 12/7/15.
+//  Copyright © 2015 matthew weintrub. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
